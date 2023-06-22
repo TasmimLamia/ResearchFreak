800d81806ee6d045b8d4d4cbf6d67b6d8f903c7e
rejwan296 <rajwan296@gmail.com>
2023-06-22 11:38:00 +0600

    add connections

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ import express, { Router } from 'express';
 import userRoutes from './userRoutes.js';
 import authRoutes from './authRoutes.js';
 import profileRoutes from './profileRoutes.js';
+import connectionsRoutes from './connectionsRoutes.js';
 
 
 const router = new Router;
@@ -10,5 +11,6 @@ const router = new Router;
 router.use('/user', userRoutes);
 router.use('/signinup', authRoutes);
 router.use('/profile', profileRoutes);
+router.use('/connections', connectionsRoutes);
 
 export default router;
